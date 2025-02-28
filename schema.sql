3ab7ca9234cf0465ecb1ef7502dbfd8cbcad71c3
Nicolai Grymer <pronicoxd@gmail.com>
2025-02-28 10:55:20 +0100

    fix db

--- schema.sql
+++ schema.sql
@@ -1,5 +1,3 @@
-create database minitwit;
-
 drop table if exists "user";
 create table "user" (
   user_id SERIAL primary key,
@@ -8,13 +6,13 @@ create table "user" (
   pw_hash VARCHAR not null
 );
 
-drop table if exists follower;
+drop table if exists "follower";
 create table follower (
   who_id integer,
   whom_id integer
 );
 
-drop table if exists message;
+drop table if exists "message";
 create table message (
   message_id SERIAL primary key,
   author_id integer not null,

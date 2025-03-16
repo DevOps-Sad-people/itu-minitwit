a2d2d405c5071fb0ae196fdad18b837bd8973790
gabortodor <gabortodor@gmail.com>
2025-03-16 08:56:53 +0100

    Test Ruby linter

--- minitwit.rb
+++ minitwit.rb
@@ -431,6 +431,13 @@ get "/latest" do
   {latest: latest_processed_command_id}.to_json
 end
 
+
+
+
+
+
+
+
 # Place this in bottom, because the routes are evaluated from top to bottom
 # e.g. /:username would match /login or /logout
 get "/:username" do

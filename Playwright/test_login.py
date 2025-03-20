47b5fced24b47bfcaf1feb29f6826039ca4cd4bf
Sebastian <andersen_sebastian@yahoo.dk>
2025-03-20 23:36:57 +0100

    Add test

---
+++ Playwright/test_login.py
@@ -0,0 +1,27 @@
+from playwright.sync_api import Page, expect
+
+
+def test_sign_up_empty_db(page: Page) -> None:
+    page.goto("http://localhost:4567/public")
+    expect(page.get_by_role("emphasis")).to_contain_text("There's no message so far.")
+    expect(page.get_by_role("heading")).to_contain_text("MiniTwit")
+    expect(page.locator("body")).to_contain_text("MiniTwit — A Ruby Sinatra Application")
+    expect(page.locator("body")).to_contain_text("sign in")
+    expect(page.locator("body")).to_contain_text("sign up")
+    expect(page.get_by_text("MiniTwit — A Ruby Sinatra")).to_be_visible()
+    expect(page.locator("body")).to_contain_text("Page 1 of 1")
+    page.get_by_role("link", name="sign up").click()
+    page.locator("input[name=\"username\"]").click()
+    page.locator("input[name=\"username\"]").fill("b")
+    page.locator("input[name=\"username\"]").press("Tab")
+    page.locator("input[name=\"email\"]").fill("a@a.dk")
+    page.locator("input[name=\"email\"]").press("Tab")
+    page.locator("input[name=\"password\"]").fill("1234")
+    page.locator("input[name=\"password\"]").press("Tab")
+    page.locator("input[name=\"password2\"]").fill("1234")
+    expect(page.get_by_role("button", name="Sign Up")).to_be_visible()
+    expect(page.get_by_role("button")).to_contain_text("Sign Up")
+    page.get_by_role("button", name="Sign Up").click()
+    expect(page.locator("body")).to_match_aria_snapshot("- heading \"Sign Up\" [level=2]\n- term: \"Username:\"\n- definition:\n  - textbox: a\n- term: \"E-Mail:\"\n- definition:\n  - textbox: a@a.dk\n- term: \"Password:\"\n- definition:\n  - textbox: /\\d+/\n- term: \"Password (repeat):\"\n- definition:\n  - textbox: /\\d+/\n- button \"Sign Up\"")
+    expect(page.get_by_role("listitem")).to_match_aria_snapshot("- listitem: You were successfully registered and can login now")
+    expect(page.get_by_text("You were successfully registered and can login now")).to_be_visible()

2f6bb6eeb69b9a84aa8d28e5b1da74f4d878f9f9
nicra <nicra@itu.dk>
2025-03-22 16:18:25 +0100

    Added new signin test

--- Playwright/test_signup.py
+++ Playwright/test_signup.py
@@ -1,7 +1,14 @@
 from playwright.sync_api import Page, expect
-
+from utils import get_random_string
 
 def test_sign_up_empty_db(page: Page) -> None:
+    
+    # Arrange
+    # Get a random string
+    random_string = get_random_string(10)
+    
+    # Go to the public page and assert the application
+    # Then click on the sign up link
     page.goto("http://localhost:4567/public")
     expect(page.get_by_role("emphasis")).to_contain_text("There's no message so far.")
     expect(page.get_by_role("heading")).to_contain_text("MiniTwit")
@@ -10,17 +17,23 @@ def test_sign_up_empty_db(page: Page) -> None:
     expect(page.locator("body")).to_contain_text("sign up")
     expect(page.get_by_text("MiniTwit — A Ruby Sinatra")).to_be_visible()
     expect(page.locator("body")).to_contain_text("Page 1 of 1")
+
+    # Click on the sign up link and fill out the form
     page.get_by_role("link", name="sign up").click()
     page.locator("input[name=\"username\"]").click()
-    page.locator("input[name=\"username\"]").fill("b")
+    page.locator("input[name=\"username\"]").fill(random_string)
     page.locator("input[name=\"username\"]").press("Tab")
-    page.locator("input[name=\"email\"]").fill("a@a.dk")
+    page.locator("input[name=\"email\"]").fill(f"{random_string}@a.dk")
     page.locator("input[name=\"email\"]").press("Tab")
-    page.locator("input[name=\"password\"]").fill("1234")
+    page.locator("input[name=\"password\"]").fill(random_string)
     page.locator("input[name=\"password\"]").press("Tab")
-    page.locator("input[name=\"password2\"]").fill("1234")
+    page.locator("input[name=\"password2\"]").fill(random_string)
+    
+    # Submit the form and sign up
     expect(page.get_by_role("button", name="Sign Up")).to_be_visible()
     expect(page.get_by_role("button")).to_contain_text("Sign Up")
     page.get_by_role("button", name="Sign Up").click()
+    
+    # Assert that the user was created
     expect(page.get_by_role("listitem")).to_contain_text("You were successfully registered and can login now")
     

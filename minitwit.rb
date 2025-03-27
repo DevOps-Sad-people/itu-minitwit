74e08b85de138f179e0b0ee6ff4f498f8a29e758
klnyzzz33 <kalny.zalan7@gmail.com>
2025-03-27 13:38:58 +0100

    Minor changes

--- minitwit.rb
+++ minitwit.rb
@@ -11,7 +11,7 @@ require "rack"
 require "prometheus/middleware/exporter"
 require "active_support/time"
 require_relative "db_migrations"
-require_relative "config"
+require_relative "prometheus_config"
 
 # configuration
 PER_PAGE = 30
@@ -20,7 +20,7 @@ DB_URL = "postgres://#{ENV.fetch("DB_USER")}:#{ENV.fetch("DB_PASSWORD")}@#{ENV.f
 
 use Rack::Deflater
 use Prometheus::Middleware::Exporter
-use MyCollector
+use PrometheusCollector
 
 configure do
   set :port, 4567

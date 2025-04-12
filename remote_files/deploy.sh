ff166c102653102c15363d8aefdb5e47a17cb88b
gabortodor <gabortodor@gmail.com>
2025-04-12 14:13:36 +0200

    Changed back configs into Dockerfile

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -2,7 +2,7 @@
 bash convert_env_into_secrets.sh
 
 # Deletes all docker configs so we recreate from the compose
-docker config ls -q | xargs -r docker config rm
+#docker config ls -q | xargs -r docker config rm
 
 docker compose -f docker-compose.yml pull
 # docker compose -f docker-compose.yml up -d

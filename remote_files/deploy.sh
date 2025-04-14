5d305f893d6575f083d0fe6f651e7fa764e12e8b
klnyzzz33 <kalny.zalan7@gmail.com>
2025-04-14 16:50:51 +0200

    Added --with-registry-auth option to docker stack deploy to correctly update swarm

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -4,4 +4,4 @@
 docker compose -f docker-compose.yml pull
 
 # Interpolate env variables for swarm, then deploy
-bash interpolate_compose_file.sh | docker stack deploy -c - minitwit
+bash interpolate_compose_file.sh | docker stack deploy -c - --with-registry-auth minitwit -d

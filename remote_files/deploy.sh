d2143dbc78267718052b4e2fdb7e1885df91f283
klnyzzz33 <kalny.zalan7@gmail.com>
2025-04-14 02:35:16 +0200

    Added elasticsearch setup for deployment

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,9 +1,6 @@
 # Converts the contents of the .env file into docker secrets
 bash convert_env_into_secrets.sh
 
-# Deletes all docker configs so we recreate from the compose
-#docker config ls -q | xargs -r docker config rm
-
 docker compose -f docker-compose.yml pull
 # docker compose -f docker-compose.yml up -d
 docker stack deploy -c docker-compose.yml minitwit

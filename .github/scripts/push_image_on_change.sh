1b59adbc844b2e058397c96a9c3a101022699e94
klnyzzz33 <kalny.zalan7@gmail.com>
2025-04-15 02:45:31 +0200

    Minor fix

--- .github/scripts/push_image_on_change.sh
+++ .github/scripts/push_image_on_change.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-source .VERSION
-
 IMAGE_NAME="${IMAGE_NAME}"
 REF_NAME=${REF_NAME}
 CONTEXT_PATH="${CONTEXT_PATH}"
 REGISTRY_NAME="${REGISTRY_NAME}"
 
 echo "Checking for changes in the image files..."
-if ! git rev-parse HEAD~1 >/dev/null 2>&1 || ! git diff --quiet HEAD HEAD~1 --; then
+if ! git rev-parse HEAD~1 >/dev/null 2>&1 || ! git diff --quiet HEAD HEAD~1 -- $CONTEXT_PATH; then
   echo "Building $IMAGE_NAME:$REF_NAME image..."
   VERSION_TAG="$REGISTRY_NAME/$IMAGE_NAME:$REF_NAME"
   LATEST_TAG="$REGISTRY_NAME/$IMAGE_NAME:latest"

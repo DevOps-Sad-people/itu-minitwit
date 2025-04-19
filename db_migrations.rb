2fe134023585771405d8718644b4f4a467aacc2d
Sebastian <andersen_sebastian@yahoo.dk>
2025-04-19 15:31:19 +0200

    Set current migration to 6

--- db_migrations.rb
+++ db_migrations.rb
@@ -6,7 +6,7 @@ Sequel.extension :migration
 def migrate_db(db)
   # Check if the database is already migrated
   if db.tables.include?(:follower) && db.tables.include?(:message) && db.tables.include?(:user)
-    Sequel::Migrator.run(db, "migrations", current: 1)
+    Sequel::Migrator.run(db, "migrations", current: 6)
     return
   end
   Sequel::Migrator.run(db, "migrations")

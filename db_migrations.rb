6d59881985042513b8405097967955b1a7f9d19e
Sebastian <andersen_sebastian@yahoo.dk>
2025-04-19 15:48:09 +0200

    Reset migration script and use environment vars for db config in compose file

--- db_migrations.rb
+++ db_migrations.rb
@@ -4,11 +4,6 @@ require "sequel"
 Sequel.extension :migration
 
 def migrate_db(db)
-  # Check if the database is already migrated
-  if db.tables.include?(:follower) && db.tables.include?(:message) && db.tables.include?(:user)
-    Sequel::Migrator.run(db, "migrations", current: 6)
-    return
-  end
   Sequel::Migrator.run(db, "migrations")
   puts "Database migrated"
 end

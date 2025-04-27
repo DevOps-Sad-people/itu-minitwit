7928d8b396ad63d944ac4e57917432b14a7357e7
klnyzzz33 <kalny.zalan7@gmail.com>
2025-04-27 19:17:14 +0200

    Compose fixes

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -5,12 +5,12 @@ TYPE=${2:-staging} # Default to 'staging' if no type is provided
 
 if [ "$TYPE" == "staging" ]; then
   # Pull images from registry
-  docker compose -f docker-compose.staging.yml pull
+  TAG=$TAG docker compose -f docker-compose.staging.yml pull
 
   TAG=$TAG docker stack deploy -c docker-compose.staging.yml --with-registry-auth minitwit -d
 else
   # Pull images from registry
-  docker compose -f docker-compose.yml pull
+  TAG=$TAG docker compose -f docker-compose.yml pull
 
   TAG=$TAG docker stack deploy -c docker-compose.yml --with-registry-auth minitwit -d
 fi

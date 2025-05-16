02bf4cde3091f31a48e99e0848b02770426c87cf
Sebastian <andersen_sebastian@yahoo.dk>
2025-05-16 23:29:10 +0200

    Update bootstrap.sh

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -99,74 +99,9 @@ ssh \
     -i ssh_key/terraform \
     'docker stack deploy minitwit -c minitwit_stack.yml --with-registry-auth'
 
-
-# #!/bin/bash
-# # This script sets up SSL for a domain using Certbot and Nginx on a fresh Ubuntu server
-# # Usage: ./upgrade-to-ssl.sh <domain> <port>
-# # Example: ./upgrade-to-ssl.sh example.com 3000
-
-# # Write out help message if no arguments are provided
-# if [ $# -ne 2 ]; then
-#     echo "Usage: $0 <domain> <port>"
-#     echo "Example: $0 example.com 3000"
-#     exit 1
-# fi
-
-# DOMAIN=$1
-# PORT=$2
-
-# # Setup CERTBOT for SSL
-# # This script is intended to be run on a fresh Ubuntu 20.04 server
-# sudo snap install core; sudo snap refresh core
-# sudo apt remove certbot
-# sudo snap install --classic certbot
-# sudo ln -s /snap/bin/certbot /usr/bin/certbot
-
-# # Setup firewall
-# ufw enable
-# ufw allow OpenSSH
-
-# # Allow existing connections to DOMAIN
-# ufw allow $PORT/tcp
-# ufw allow $PORT/udp
-
-# # Install Nginx
-# sudo apt update
-# sudo apt install nginx
-
-# # Setup nginx config
-# # /etc/nginx/sites-available/$DOMAIN
-# echo "server {
-#     listen 80;
-#     server_name $DOMAIN www.$DOMAIN;
-
-#     # This location allows Let's Encrypt HTTP verification
-#     location /.well-known/acme-challenge/ {
-#         root /var/www/html;
-#     }
-
-#     location / {
-#         proxy_pass http://127.0.0.1:$PORT;
-#         proxy_set_header Host \$host;
-#         proxy_set_header X-Real-IP \$remote_addr;
-#         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-#     }
-# }" | sudo tee /etc/nginx/sites-available/$DOMAIN > /dev/null
-
-# sudo mkdir -p /var/www/html/.well-known/acme-challenge
-# sudo chmod -R 755 /var/www/html
-
-# sudo ln -s /etc/nginx/sites-available/$DOMAIN /etc/nginx/sites-enabled/
-
-# sudo nginx -t
-# sudo systemctl reload nginx
-
-# sudo ufw allow 'Nginx Full'
-# sudo ufw delete allow 'Nginx HTTP'
-
-# echo -e "\n--> Done bootstrapping Minitwit"
-# echo -e "--> The dbs will need a moment to initialize, this can take up to a couple of minutes..."
-# echo -e "--> Site will be avilable @ http://$(terraform output -raw public_ip):4567"
-# echo -e "--> You can check the status of swarm cluster @ http://$(terraform output -raw minitwit-swarm-leader-ip-address):8888"
-# echo -e "--> ssh to swarm leader with 'ssh root@\$(terraform output -raw minitwit-swarm-leader-ip-address) -i ssh_key/terraform'"
-# echo -e "--> To remove the infrastructure run: bash destroy.sh"
+echo -e "\n--> Done bootstrapping Minitwit"
+echo -e "--> The dbs will need a moment to initialize, this can take up to a couple of minutes..."
+echo -e "--> Site will be avilable @ http://$(terraform output -raw public_ip):4567"
+echo -e "--> You can check the status of swarm cluster @ http://$(terraform output -raw minitwit-swarm-leader-ip-address):8888"
+echo -e "--> ssh to swarm leader with 'ssh root@\$(terraform output -raw minitwit-swarm-leader-ip-address) -i ssh_key/terraform'"
+echo -e "--> To remove the infrastructure run: bash destroy.sh"
